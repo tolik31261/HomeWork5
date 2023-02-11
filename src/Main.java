93e2ca39564955bda49d10474d84a298e2eaf499
User <nazarovtolya2@gmail.com>
2023-02-11 11:35:28 +0500

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-
+        int clientOs = 1;
+        if (clientOs == 0) {
+            System.out.println("Для установки приложения для iOS перейдите по ссылке");
+        } else if (clientOs == 1) {
+            System.out.println("Для установки приложения для Android перейдите по ссылке");
+        } else {
+            System.out.println("Ваша операционная система не поддерживается приложением");
+        }
     }
 }
