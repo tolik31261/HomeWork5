aed8aae99c1afacd864c4fc9ef0fed7e157231cf
User <nazarovtolya2@gmail.com>
2023-02-12 19:14:08 +0500

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -12,6 +13,23 @@ public class Main {
             System.out.println("Для установки приложения для Android перейдите по ссылке");
         } else {
             System.out.println("Ваша операционная система не поддерживается приложением");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2015;
+        int clientOs = 1;
+        if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите приложение для iOS по ссылке");
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else if (clientOs == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите приложение для Android по ссылке");
+        } else {
+            System.out.println("Ваша операционная система не поддерживается.");
         }
     }
 }
